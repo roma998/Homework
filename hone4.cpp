8837b3fc771b9a5becd5eab2ce5ad5010536343f
roma998 <65311050+roma998@users.noreply.github.com>
2025-09-24 12:46:36 +0300

    Update hone4.cpp

--- hone4.cpp
+++ hone4.cpp
@@ -1,10 +1,44 @@
-#include <iostream>
-#include <string>
+﻿#include <iostream>
 
+void intToCharArray(int number, char* result)
+{
+    
+    char temp[12];
+    int index=0;
+    int resultIndex = 0;
+    if (number == 0)
+    {
+        result[0] = '0';
+        result[1] = '\0';
+        return;
+    }
+    
+    bool negativ = number < 0;
+    if (negativ)
+    {
+        number = -number;
+        result[0] = '-';
+        resultIndex = 1;
+
+    }
+    while (number>0)
+    {
+        temp[index] = '0' + number % 10;
+        ++index;
+        number /= 10;
+    }
+    
+    for (int i=0; index > i;++i)
+    {
+        result[i+ resultIndex] = temp[index- 1 - i];
+    }
+    result[index+ resultIndex] = '\0';
+}
 int main()
 {
-int number;
-std::cin >> number;
-    std::string str = std::to_string(number);
-std::cout<<str;
+    int number = -623;
+    char result[12];
+    intToCharArray(number, result);
+    std::cout << "Число " << number << " у вигляді рядка: " << result << std::endl;
+    return 0;
 }
